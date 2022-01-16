5fd5ad749d33229f72bd27aee4c3eba8a4b35600
Vidottizzz <vidottizzz@gmail.com>
2022-01-15 22:45:11 -0300

    fixing img disposition

--- script.js
+++ script.js
@@ -6,31 +6,53 @@ const allInput = document.querySelectorAll('.input');
 const formBtn = document.querySelector('.form-btn');
 const inptEmail = document.querySelector('#email');
 const errImg = document.querySelectorAll('.err-img');
-
+const pError = document.querySelectorAll('.p-error');
+const pEmailError = document.querySelector('.p-email-error');
 
 
 formBtn.addEventListener('click', (e) => {
 
     for (i = 0; i < allInput.length; i++) {
         e.preventDefault();
-      let allInputs = allInput[i];
-      let allImgs = errImg[i];
+        let allInputs = allInput[i];
+        let allImgs = errImg[i];
+        let allP = pError[i];
 
-        if(allInputs.value === "") {
+        if (allInputs.value === "") {
             allInput[i].classList.add('back-red');
             allImgs.classList.add('show');
             allImgs.classList.remove('hide');
-
-            
-        } else{      
+            allP.classList.add('show');
+            allP.classList.remove('hide');
+        } else {
             allInputs.classList.remove('back-red');
             allImgs.classList.remove('show');
             allImgs.classList.add('hide');
+            allP.classList.add('hide');
+            allP.classList.remove('show');
         }
 
     }
-    if ( inptEmail.value.indexOf('@') == -1 || inptEmail.value.indexOf('.') == -1) {
-        console.log('não tem arroba e nem pontos')
+
+
+
+
+    if (inptEmail.value.indexOf('@') == -1 || inptEmail.value.indexOf('.') == -1) {
+        pEmailError.classList.add('show-display');
+        pEmailError.classList.remove('hide-display');
+
+        allInput[i].classList.add('back-red');
+        allImgs.classList.add('show');
+        allImgs.classList.remove('hide');
+
+
+    } else {
+        pEmailError.classList.add('hide');
+        pEmailError.classList.remove('show');
+        allInputs.classList.remove('back-red');
+        allImgs.classList.remove('show');
+        allImgs.classList.add('hide');
+
     }
 })
 
